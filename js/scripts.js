88665f97e6e6bbb2d71919a74abe7bf081297f6e
Someone942 <107355068+someone942@users.noreply.github.com>
2023-10-26 16:01:25 +0200

    Rewrite the playRound and game function
    The playRound function now returns a value for -1 to 1 for either a
    Lose, Tie or Win. Removing the log messages and transferring them to
    the game function.
    The game function will keep track to the current score and print
    a message accordingly to the end result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 const CHOICES = ["Rock", "Paper", "Scissors"];
+let playerWon = 0;
+let computerWon = 0;
 
 function capitalizeFirstLetter(string) {
     return string.slice(0, 1).toUpperCase() + string.slice(1).toLowerCase();
@@ -8,27 +10,23 @@ function getComputerChoice() {
     return CHOICES[Math.floor(Math.random() * 3)];
 }
 
+/* The function compares the playerSelection with the computerSelection
+and will return either 0 for a tie, 1 for a win or -1 for a loss */
 function playRound(playerSelection, computerSelection) {
-    playerWon = false;
     lowerCasePlayerSelection = playerSelection.toLowerCase();
     lowerCaseComputerSelection = computerSelection.toLowerCase();
 
     if (lowerCaseComputerSelection === lowerCasePlayerSelection) {
-        return `Tie! Player: ${capitalizeFirstLetter(lowerCasePlayerSelection)} vs. Computer: ${capitalizeFirstLetter(lowerCaseComputerSelection)}`
+        return 0
     }
     
     if ((lowerCaseComputerSelection === "paper" && lowerCasePlayerSelection === "scissors")
         || (lowerCaseComputerSelection === "rock" && lowerCasePlayerSelection === "paper")
         || (lowerCaseComputerSelection === "scissors" && lowerCasePlayerSelection === "rock")) {
-        playerWon = true;        
+        return 1
     }
 
-    if (playerWon) {
-        return `You Win! Player: ${capitalizeFirstLetter(lowerCasePlayerSelection)} vs. Computer: ${capitalizeFirstLetter(lowerCaseComputerSelection)}`
-    }
-    else {
-        return `You Lose! Player: ${capitalizeFirstLetter(lowerCasePlayerSelection)} vs. Computer: ${capitalizeFirstLetter(lowerCaseComputerSelection)}`
-    }
+    return -1
 }
 
 function getPlayerSelection() {
@@ -37,7 +35,31 @@ function getPlayerSelection() {
 
 function game() {
     for (i = 0; i <= 4; i++) {
-        console.log(playRound(getPlayerSelection(), getComputerChoice()));
+        result = playRound(getPlayerSelection(), getComputerChoice())
+
+        switch (result) {
+            case -1:
+                message = `You Lose! Player: ${capitalizeFirstLetter(lowerCasePlayerSelection)} vs. Computer: ${capitalizeFirstLetter(lowerCaseComputerSelection)}`;
+                computerWon += 1;
+                break;
+            case 0:
+                message = `Tie! Player: ${capitalizeFirstLetter(lowerCasePlayerSelection)} vs. Computer: ${capitalizeFirstLetter(lowerCaseComputerSelection)}`;
+                break;
+            case 1:
+                message = `You Win! Player: ${capitalizeFirstLetter(lowerCasePlayerSelection)} vs. Computer: ${capitalizeFirstLetter(lowerCaseComputerSelection)}`;
+                playerWon += 1;
+                break;
+        }
+        console.log(message);
+    }
+    if (playerWon > computerWon) {
+        console.log(`You win the game! Player: ${playerWon} Computer: ${computerWon}`)
+    }
+    else if (playerWon === computerWon) {
+        console.log(`Game Tie! Player: ${playerWon} Computer: ${computerWon}`)
+    }
+    else {
+        console.log(`You lose the game! Player: ${playerWon} Computer: ${computerWon}`)
     }
 }
 
